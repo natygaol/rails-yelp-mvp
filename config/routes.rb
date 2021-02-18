27696ed97b64cf0e9779f0607cf24201beb0a67c
nataly garcia <naty_garcioli@hotmail.com>
2021-02-18 12:52:07 -0500

    pending

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants #do
-    # collection do
-    #   get :top
-    # end
-  #end
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
 end
